bb3f0be9bfe43f0598370872e405a03ce8d92014
artender <artem1347929@gmail.com>
2025-05-02 22:11:24 +0300

    ✨ Add proper replies for /start command

--- bot/handlers/commands.py
+++ bot/handlers/commands.py
@@ -2,13 +2,15 @@
 from telebot.async_telebot import AsyncTeleBot
 from telebot.types import Message
 from .utils.markup import get_admin_markup, get_user_markup
+from .replies import START_CMD_REPLY
 
 def register_command_handlers(bot: AsyncTeleBot) -> None:
     @bot.message_handler(is_admin=True, commands=["start"]) # type: ignore[misc]
     async def handle_start_command_for_admins(msg: Message) -> None:
         await bot.send_message(
             msg.chat.id,
-            "Hello, admin!",
+            START_CMD_REPLY,
+            parse_mode="MarkdownV2",
             reply_markup=get_admin_markup()
         )
 
@@ -16,6 +18,7 @@ def register_command_handlers(bot: AsyncTeleBot) -> None:
     async def handle_start_command_for_users(msg: Message) -> None:
         await bot.send_message(
             msg.chat.id,
-            "Hello, user!",
+            START_CMD_REPLY,
+            parse_mode="MarkdownV2",
             reply_markup=get_user_markup()
         )
---
+++ bot/handlers/replies/__init__.py
@@ -0,0 +1,3 @@
+from .commands import START_CMD_REPLY
+
+__all__ = ["START_CMD_REPLY"]
