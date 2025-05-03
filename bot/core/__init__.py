eb59fbda11448e7b7fe7bf9c16ac2071a1af6330
artender <artem1347929@gmail.com>
2025-05-03 18:25:43 +0300

    ✨ Add state filter

--- bot/core/__init__.py
+++ bot/core/__init__.py
@@ -1,10 +1,12 @@
 # mypy: disable-error-code="import-untyped"
 from telebot.async_telebot import AsyncTeleBot
-from .custom_filters import AdminFilter
 from .config import bot_config as config
 from .storage import get_redis
 
 __all__ = ["config", "get_redis"]
 
 def register_custom_filters(bot: AsyncTeleBot) -> None:
-    bot.add_custom_filter(AdminFilter())
+    from .custom_filters import AdminFilter, StateFilter
+    custom_filters = (AdminFilter, StateFilter)
+    for custom_filter in custom_filters:
+        bot.add_custom_filter(custom_filter())
--- bot/core/custom_filters.py
+++ bot/core/custom_filters.py
@@ -1,7 +1,9 @@
 # mypy: disable-error-code="import-untyped"
 from telebot.types import Message
 from telebot.asyncio_filters import SimpleCustomFilter
+from telebot.asyncio_filters import AdvancedCustomFilter
 from .config import bot_config
+from models import UserSession
 
 class AdminFilter(SimpleCustomFilter): # type: ignore[misc]
     key = "is_admin"
@@ -10,3 +12,13 @@ class AdminFilter(SimpleCustomFilter): # type: ignore[misc]
     async def check(msg: Message) -> bool:
         user_id = msg.from_user.id
         return user_id in bot_config.admin_user_ids
+
+class StateFilter(AdvancedCustomFilter): # type: ignore[misc]
+    key = "state"
+
+    @staticmethod
+    async def check(msg: Message, text: str) -> bool:
+        user_id = msg.from_user.id
+        session = UserSession(user_id)
+        state = await session.get_state()
+        return state == text
