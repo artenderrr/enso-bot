f48d0a660b1e44948389936c73bdb325658dd72f
artender <artem1347929@gmail.com>
2025-05-04 18:52:11 +0300

    ✨ Add 3rd stage of add_item

--- bot/handlers/messages/add_item.py
+++ bot/handlers/messages/add_item.py
@@ -7,7 +7,9 @@ from ..replies import (
     REJECT_MSG,
     ADD_ITEM_MSG_START_SUCCESS,
     ADD_ITEM_MSG_NAME_FAILURE,
-    ADD_ITEM_MSG_NAME_SUCCESS
+    ADD_ITEM_MSG_NAME_SUCCESS,
+    ADD_ITEM_MSG_COLLECTION_FAILURE,
+    ADD_ITEM_MSG_COLLECTION_SUCCESS
 )
 
 def register_add_item_handlers(bot: AsyncTeleBot) -> None:
@@ -41,4 +43,24 @@ def register_add_item_handlers(bot: AsyncTeleBot) -> None:
             await data["session"].set_state("add_item:collection")
             await bot.send_message(
                 msg.chat.id, ADD_ITEM_MSG_NAME_SUCCESS, parse_mode="MarkdownV2"
+            )
+
+    @bot.message_handler(
+        is_admin=True,
+        state="add_item:collection",
+        content_types=content_type_media,
+        func=lambda msg: True
+    ) # type: ignore[misc]
+    async def handle_add_item_collection(msg: Message, data: dict[Any, Any]) -> None:
+        if msg.content_type != "text" or len(msg.text) > 64:
+            await bot.reply_to(
+                msg, ADD_ITEM_MSG_COLLECTION_FAILURE, parse_mode="MarkdownV2"
+            )
+        else:
+            await data["session"].update_context(
+                {"item_collection": msg.text}
+            )
+            await data["session"].set_state("add_item:volume")
+            await bot.send_message(
+                msg.chat.id, ADD_ITEM_MSG_COLLECTION_SUCCESS, parse_mode="MarkdownV2"
             )
