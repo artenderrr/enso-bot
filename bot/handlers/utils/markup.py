009e41753e4dae2dbf899f625767ae85577a2da2
artender <artem1347929@gmail.com>
2025-05-07 14:56:56 +0300

    ✨ Add get_view_items_markup()

--- bot/handlers/utils/markup.py
+++ bot/handlers/utils/markup.py
@@ -1,5 +1,6 @@
 # mypy: disable-error-code="import-untyped"
 from telebot.types import ReplyKeyboardMarkup, KeyboardButton
+from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 def create_markup_row(*args: str) -> list[KeyboardButton]:
     return [KeyboardButton(button_text) for button_text in args]
@@ -18,4 +19,12 @@ def get_admin_markup() -> ReplyKeyboardMarkup:
 def get_user_markup() -> ReplyKeyboardMarkup:
     markup = ReplyKeyboardMarkup(resize_keyboard=True)
     markup.add(KeyboardButton("Оставить отзыв"))
+    return markup
+
+def get_view_items_markup() -> InlineKeyboardMarkup:
+    markup = InlineKeyboardMarkup(row_width=2)
+    markup.add(
+        InlineKeyboardButton("←", callback_data="view_items:backward"),
+        InlineKeyboardButton("→", callback_data="view_items:forward")
+    )
     return markup
